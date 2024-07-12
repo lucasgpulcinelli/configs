de0ca422ff887213edf90dd1a4405151bf34b961
lucasgpulcinelli <lucasegp@usp.br>
2024-07-12 04:36:46 -0300

    humm I love windows 11 VMs, they require fake or bypassed TPM

--- configuration.nix
+++ configuration.nix
@@ -115,7 +115,25 @@
 
   virtualisation = {
     containers.enable = true;
-    libvirtd.enable = true;
+
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            })
+            .fd
+          ];
+        };
+      };
+    };
 
     docker = {enable = true;};
   };
